faf35109b3e59199023690dbf1777402b633a67e
Ben Burkert <ben@benburkert.com>
2012-08-21 11:54:55 -0700

    Update $PS1.

---
+++ script/prompt.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/'
+}
+
+function rbenv_version {
+  local version=$(rbenv local 2>/dev/null)
+
+  if [[ $? -eq 0 ]] ; then
+    echo -ne "($version)"
+  fi
+}
+
+function set_prompt {
+  local         RED="\[\033[0;31m\]"
+  local        BLUE="\[\033[0;34m\]"
+  local       GREEN="\[\033[0;32m\]"
+  local       WHITE="\[\033[1;37m\]"
+  local  LIGHT_GRAY="\[\033[0;37m\]"
+  local       CLEAR="\[\033[m\]"
+
+  PS1="$WHITE\w$BLUE\${rbenv_version}$GREEN\${parse_git_branch}${RED}  $CLEAR"
+  PS2='> '
+  PS4='+ '
+}
+
+export -f set_prompt
