8b410a2ad61000c60590f81af38bb5910eba3110
Ben Burkert <ben@benburkert.com>
2012-10-30 12:15:01 -0700

    Add some emacs packages, set the default theme.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,3 +2,31 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+(defvar my-packages '(
+  starter-kit
+  starter-kit-bindings
+  starter-kit-lisp
+  starter-kit-ruby
+  clojure-mode
+  markdown-mode
+  color-theme-solarized))
+
+(dolist (pkg my-packages)
+  (when (not (package-installed-p pkg))
+    (package-install pkg)))
+
+(load-theme 'solarized-dark t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
