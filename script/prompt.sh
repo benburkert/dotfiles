8cdd0a136abbb155e3084aae0b38c3191a3736c3
Ben Burkert <ben@benburkert.com>
2014-05-28 16:43:55 -0700

    Checkin the multi-line prompt.

--- script/prompt.sh
+++ script/prompt.sh
@@ -12,15 +12,29 @@ function rbenv_version {
   fi
 }
 
+function previous_exit_status {
+  echo -ne "$?"
+}
+
 function set_prompt {
+  local exit_status=$?
+
   local         RED="\[\033[0;31m\]"
   local        BLUE="\[\033[0;34m\]"
-  local       GREEN="\[\033[0;32m\]"
+  local       GREEN="\[\033[1;32m\]"
   local       WHITE="\[\033[1;37m\]"
+  local      YELLOW="\[\033[0;33m\]"
+  local      PURPLE="\[\033[0;35m\]"
   local  LIGHT_GRAY="\[\033[0;37m\]"
   local       CLEAR="\[\033[m\]"
 
-  PS1="$WHITE\w$BLUE\$(rbenv_version)$GREEN\$(parse_git_branch)${RED}  $CLEAR"
+  if [[ $exit_status -eq 0 ]] ; then
+    local exit_color=$GREEN
+  else
+    local exit_color=$RED
+  fi
+
+  PS1="${YELLOW}╭──$WHITE\w$BLUE\$(rbenv_version)$PURPLE\$(parse_git_branch)${exit_color}\n${YELLOW}╰→ $CLEAR"
   PS2='> '
   PS4='+ '
 }
